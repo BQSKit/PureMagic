b8cfa36fe9b00c4b1e4dbd3c37834c9b00712d9b
Steven Hofmeyr <shofmeyr@lbl.gov>
2025-04-16 15:50:49 -1000

    Experimenting with Steiner trees

--- lssp.py
+++ lssp.py
@@ -75,13 +75,34 @@ def build_parallel_topo(num_cols, num_rows):
     print("  data: ", num_data_qubits)
     print("  bus:  ", num_bus_qubits)
     print("Space efficiency: %.2f" % (float(num_data_qubits) / (num_data_qubits + num_bus_qubits)))
+    print("Magic state ratio: %.2f" % (float(num_magic_qubits) / (num_data_qubits + num_magic_qubits)))
+    topo_fname = "lssp-topo"
+    print("Plotting topology to", topo_fname, "...")
     # print("Generated topology with", num_qubits, "data qubits and ")
     plt.rc("figure", figsize=[num_cols, num_rows])
     nx.draw_networkx(topo_graph, pos=node_pos, node_size=1000, node_color=node_colors, font_size=10)
     nx.draw_networkx_edge_labels(topo_graph, node_pos, edge_labels, rotate=False)
     plt.tight_layout()
-    plt.savefig("lssp.pdf")
-    plt.savefig("lssp.png")
+    plt.savefig(topo_fname + ".pdf")
+    plt.savefig(topo_fname + ".png")
+
+    terminal_nodes = []
+    terminal_nodes.append("m0-0")
+    # node_pos = {}
+    for node in topo_graph.nodes:
+        if node[0] == "d":
+            terminal_nodes.append(node)
+            col, row = node[1:].split("-")
+            # node_pos[node] = [int(col), num_rows - 1 - int(row)]
+    print(terminal_nodes)
+    steiner_graph = nx.algorithms.approximation.steiner_tree(topo_graph, terminal_nodes)
+    stree_fname = "lssp-steiner"
+    plt.clf()
+    nx.draw_networkx(steiner_graph, pos=node_pos, node_size=1000, font_size=10)
+    # nx.draw_networkx_edge_labels(steiner_graph, node_pos, edge_labels, rotate=False)
+    plt.tight_layout()
+    plt.savefig(stree_fname + ".pdf")
+    return num_data_qubits
 
 
 def gen_rnd_circuit(rseed, num_qubits, qubits_per_operator, num_operators):
@@ -91,30 +112,39 @@ def gen_rnd_circuit(rseed, num_qubits, qubits_per_operator, num_operators):
     operators = []
     basis_options = ["X", "Z", "Y"]
     counts = []
-    for i in range(num_operators):
+    for _ in range(num_operators):
         operator = []
         # this is a hack to ensure only positive numbers for the normal sampling
         for _ in range(100):
             operator_qubits = int(np.floor(rng.normal(mean_qubits, sigma_qubits)))
-            if operator_qubits > 0 and operator_qubits <= 10:
+            if operator_qubits > 0 and operator_qubits <= num_qubits:
                 break
         else:
             print("Couldn't generate a random number in range [0, %d], using %d" % (num_qubits, mean_qubits), file=sys.stderr)
-            operator_qubits = 1
+            operator_qubits = mean_qubits
         for _ in range(operator_qubits):
             operator.append(basis_options[int(np.floor(rng.uniform(0, 3)))])
         operators.append(operator)
         counts.append(len(operator))
 
-    for operator in operators:
-        print(operator)
-    # _, bins, _ = plt.hist(counts, 10, density=True)
-    # plt.plot(bins, 1 / (sigma_qubits * np.sqrt(2 * np.pi)) * np.exp(-((bins - mean_qubits) ** 2) / (2 * sigma_qubits**2)))
-    # plt.tight_layout()
-    # plt.show()
+    # for operator in operators:
+    #    print(operator)
+    hist_fname = "lssp-operator-freqs"
+    print("Plotting circuit histogram to", hist_fname, "...")
+    plt.clf()
+    plt.rcParams.update({"font.size": 20})
+    plt.xlabel("number of qubits")
+    plt.ylabel("Frequency")
+    _, bins, _ = plt.hist(counts, num_qubits, density=True)
+    # counts, bins = np.histogram(counts, 20)
+    density = 1 / (sigma_qubits * np.sqrt(2 * np.pi)) * np.exp(-((bins - mean_qubits) ** 2) / (2 * sigma_qubits**2))
+    plt.plot(bins, density)
+    plt.tight_layout()
+    plt.savefig(hist_fname + ".pdf")
+    plt.savefig(hist_fname + ".png")
 
 
-if __name__ == "__main__":
+def get_args():
     parser = argparse.ArgumentParser(description="Experimental scheduler for the LSSP")
     parser.add_argument("--min-num-qubits", "-n", type=int, default=10, help="Minimum number of data qubits")
     parser.add_argument(
@@ -128,14 +158,25 @@ if __name__ == "__main__":
     parser.add_argument("--rnd-seed", "-r", type=int, default=29, help="Random seed")
     args = parser.parse_args()
     print("Arguments:\n ", "\n  ".join(f"{k}={v}" for k, v in vars(args).items()))
+    return args
 
-    # gen_rnd_circuit(args.rnd_seed, args.num_qubits, args.qubits_per_operator, args.num_operators)
+
+def get_topo_dims(min_num_qubits):
     # the rows dimension needs to be a multiple of 3, and a minimum of 6
     # the columns dimension needs to be a multiple of 2, with 1 added (so 3, 5, 7, 9, ...)
-    sq_dim = int(np.floor(np.sqrt(args.min_num_qubits)))
+    sq_dim = int(np.floor(np.sqrt(min_num_qubits)))
     patch_rows = int(sq_dim / 2) + sq_dim % 2
     num_rows = 3 * patch_rows + 3
     qubits_per_col = 2 * patch_rows
-    num_cols = 2 * int(np.ceil(args.min_num_qubits / qubits_per_col)) + 1
+    num_cols = 2 * int(np.ceil(min_num_qubits / qubits_per_col)) + 1
     print("Layout dimensions:", num_cols, num_rows)
-    build_parallel_topo(num_cols, num_rows)
+    return num_cols, num_rows
+
+
+if __name__ == "__main__":
+    args = get_args()
+    num_cols, num_rows = get_topo_dims(args.min_num_qubits)
+    num_data_qubits = build_parallel_topo(num_cols, num_rows)
+    if num_data_qubits != args.min_num_qubits:
+        print("Adjusted number of data qubits from", args.min_num_qubits, "to", num_data_qubits)
+    gen_rnd_circuit(args.rnd_seed, num_data_qubits, args.qubits_per_operator, args.num_operators)
