c0c6729e5be4b775be9101b9d53372343cc3d3ed
Steven Hofmeyr <shofmeyr@lbl.gov>
2025-04-25 09:53:52 -1000

    Moved topograph to separate file

--- lssp.py
+++ lssp.py
@@ -11,6 +11,8 @@ import copy
 import time
 import functools
 import multiprocessing as mp
+import topograph
+from topograph import is_bus_node, is_data_node, is_magic_node
 
 
 def timer(func):
@@ -75,25 +77,6 @@ def get_topo_dims():
     num_cols = 2 * int(np.ceil(args.min_num_qubits / qubits_per_col)) + 1
     print("Layout dimensions:", num_cols, num_rows)
     return num_cols, num_rows
-
-
-def is_magic_node(node):
-    assert node[0] in ["m", "b", "d"]
-    return node[0] == "m"
-
-
-def is_data_node(node):
-    assert node[0] in ["m", "b", "d"]
-    return node[0] == "d"
-
-
-def is_bus_node(node):
-    assert node[0] in ["m", "b", "d"]
-    return node[0] == "b"
-
-
-def get_node_label(label, col, row):
-    return label + str(math.ceil(col)) + "-" + str(math.ceil(row))
 
 
 class PauliProduct:
@@ -180,74 +163,6 @@ def plot_topology(topo_graph, topo_fname, pauli_product_paths=[], title_str=""):
     plt.tight_layout()
     plt.savefig(topo_fname + ".pdf")
     plt.savefig(topo_fname + ".png")
-
-
-class TopoGraph(nx.Graph):
-    def __init__(self, num_cols=0, num_rows=0):
-        nx.Graph.__init__(self)
-        self.num_cols = num_cols
-        self.num_rows = num_rows
-        if num_cols > 0 and num_rows > 0:
-            self.gen_topo()
-
-    @timer
-    def gen_topo(self):
-        for col in range(self.num_cols):
-            if col % 2 != 0:
-                continue
-            node_label = self.add_labeled_node("m", col, 0)
-            self.add_edge(node_label, get_node_label("b", col, 1))
-            for row in range(1, self.num_rows - 2):
-                node_label = self.add_labeled_node("b", col, row)
-                self.add_edge(node_label, get_node_label("b", col, row + 1))
-            prev_node_label = self.add_labeled_node("b", col, self.num_rows - 2)
-            node_label = self.add_labeled_node("m", col, self.num_rows - 1)
-            self.add_edge(node_label, prev_node_label)
-        qi = 0
-        for col in range(self.num_cols):
-            if col % 2 == 0:
-                continue
-            for row in range(1, self.num_rows - 1):
-                if row % 3 == 1:
-                    node_label = self.add_labeled_node("b", col, row)
-                    self.add_edge(node_label, get_node_label("b", col - 1, row))
-                    self.add_edge(node_label, get_node_label("b", col + 1, row))
-                else:
-                    if row % 3 == 2:
-                        node_label1 = "d" + str(int(qi / 2)) + "X"
-                        node_label2 = "d" + str(int(qi / 2) + 1) + "X"
-                        self.add_edge(get_node_label("b", col, row + 2), node_label1)
-                        self.add_edge(get_node_label("b", col, row + 2), node_label2)
-                    else:
-                        node_label1 = "d" + str(int(qi / 2) - 1) + "Z"
-                        node_label2 = "d" + str(int(qi / 2)) + "Z"
-                        self.add_edge(get_node_label("b", col, row - 2), node_label1)
-                        self.add_edge(get_node_label("b", col, row - 2), node_label2)
-                    self.add_node(node_label1, pos=[float(col) - 0.35, self.num_rows - 1 - row], color="#9999FF")
-                    self.add_edge(get_node_label("b", col - 1, row), node_label1)
-                    self.add_node(node_label2, pos=[float(col) + 0.35, self.num_rows - 1 - row], color="#9999FF")
-                    self.add_edge(get_node_label("b", col + 1, row), node_label2)
-                    qi += 2
-
-        num_data_qubits = int(sum([is_data_node(node) for node in self.nodes]) / 2)
-        num_magic_qubits = sum([is_magic_node(node) for node in self.nodes])
-        num_bus_qubits = sum([is_bus_node(node) for node in self.nodes])
-        print("Number of qubits:")
-        print("  magic:", num_magic_qubits)
-        print("  data: ", num_data_qubits)
-        print("  bus:  ", num_bus_qubits)
-        print("Space efficiency: %.2f" % (float(num_data_qubits) / (num_data_qubits + num_bus_qubits)))
-        print("Magic state ratio: %.2f" % (float(num_magic_qubits) / (num_data_qubits + num_magic_qubits)))
-        self.num_data_qubits = num_data_qubits
-        self.num_magic_qubits = num_magic_qubits
-        self.num_bus_qubits = num_bus_qubits
-
-    def add_labeled_node(self, label, col, row):
-        # node_colors = {"m": "#FFBB99", "b": "#B3FFBF", "d": "#9999FF"}
-        node_colors = {"m": "#FFBB99", "b": "#cccccc", "d": "#9999FF"}
-        node_label = get_node_label(label, col, row)
-        self.add_node(node_label, pos=[col, self.num_rows - 1 - row], color=node_colors[label])
-        return node_label
 
 
 @timer
@@ -663,7 +578,7 @@ def main():
     print("Running on", num_ranks, "cores")
     rng = np.random.default_rng(seed=args.rseed)
     num_cols, num_rows = get_topo_dims()
-    topo_graph = TopoGraph(num_cols, num_rows)
+    topo_graph = topograph.TopoGraph(num_cols, num_rows)
     if topo_graph.num_data_qubits != args.min_num_qubits:
         print("Adjusted number of data qubits from", args.min_num_qubits, "to", topo_graph.num_data_qubits)
     circuit = gen_rnd_circuit(rng, topo_graph.num_data_qubits)
