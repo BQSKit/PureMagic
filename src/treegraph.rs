d9173755c65d0d0acacb5939609601780a773279
Steven Hofmeyr <shofmeyr@lbl.gov>
2026-02-18 15:07:46 -1000

    Keep track of data nodes explicitly in the tree graph to work properly with bus routing. Fixed bug in bus routing.

--- src/treegraph.rs
+++ src/treegraph.rs
@@ -1,17 +1,23 @@
 use crate::debug_sched;
+use crate::node::{Node, NodeType};
+#[allow(unused_imports)]
 use crate::utils::{_BLUE, _RESET};
 
 #[derive(Clone)]
 struct TreeNode {
     pub nbors: Vec<usize>,
     pub is_routing: bool,
+    pub is_data: bool,
     // position is needed so that we can determine which links are top or bottom
     pub pos: (f32, f32),
 }
 
 impl TreeNode {
-    pub fn new(is_routing: bool, pos: (f32, f32)) -> Self {
-        TreeNode { nbors: Vec::new(), is_routing: is_routing, pos: pos }
+    pub fn new(node: &Node) -> Self {
+        TreeNode { nbors: Vec::new(),
+                   is_routing: node.is_routing(),
+                   is_data: node.node_type == NodeType::Data,
+                   pos: node.pos }
     }
 
     pub fn remove_edge(&mut self, nb_id: usize) {
@@ -54,17 +60,38 @@ impl TreeGraph {
         self.nodes[node_id].as_ref().map(|node| node.nbors.len()).unwrap_or(0)
     }
 
-    pub fn add_node(&mut self, id: usize, is_routing: bool, pos: (f32, f32)) {
-        assert!(self.nodes[id].is_none());
-        self.nodes[id] = Some(TreeNode::new(is_routing, pos));
+    pub fn add_node(&mut self, node: &Node) {
+        assert!(self.nodes[node.id].is_none());
+        self.nodes[node.id] = Some(TreeNode::new(node));
         self.num_nodes += 1;
-        debug_sched!("      {}add node {}{}", _BLUE, id, _RESET);
+        debug_sched!("      {}add node {}{}", _BLUE, node.id, _RESET);
     }
 
     pub fn add_edge(&mut self, node_id1: usize, node_id2: usize) {
         // make sure the edge doesn't already exist
-        assert!(!self.nodes[node_id1].as_ref().unwrap().nbors.contains(&node_id2));
-        assert!(!self.nodes[node_id2].as_ref().unwrap().nbors.contains(&node_id1));
+        #[cfg(debug_assertions)]
+        {
+            let node1 = self.nodes[node_id1].as_ref().unwrap();
+            let node1_type = if node1.is_data {
+                "data"
+            } else {
+                if !node1.is_routing { "magic" } else { "bus" }
+            };
+            let node2 = self.nodes[node_id2].as_ref().unwrap();
+            let node2_type = if node2.is_data {
+                "data"
+            } else {
+                if !node2.is_routing { "magic" } else { "bus" }
+            };
+            debug_sched!("adding edge {}->{} {} {} {}",
+                         node_id1,
+                         node_id2,
+                         node1_type,
+                         node2_type,
+                         if node1.nbors.contains(&node_id2) { "DUPLICATE" } else { "" });
+            debug_assert!(!node1.nbors.contains(&node_id2));
+            debug_assert!(!node2.nbors.contains(&node_id1));
+        }
         self.nodes[node_id1].as_mut().unwrap().nbors.push(node_id2);
         self.nodes[node_id2].as_mut().unwrap().nbors.push(node_id1);
         self.num_edges += 1;
@@ -116,7 +143,7 @@ impl TreeGraph {
         let mut edges_to_remove: Vec<(usize, usize)> = Vec::new();
         for (node_id, node_opt) in self.nodes.iter().enumerate() {
             if let Some(node) = node_opt {
-                if !node.is_routing && node.nbors.len() == 2 {
+                if node.is_data && node.nbors.len() == 2 {
                     // if the nb has only one vertical edge, then remove the vertical edge,
                     // otherwise remove the side edge
                     // remove side edge
@@ -197,7 +224,7 @@ impl TreeGraph {
         let mut below_count = 0;
         for nb_id in &node.nbors {
             let nb = self.nodes[*nb_id].as_ref().unwrap();
-            if !nb.is_routing {
+            if nb.is_data {
                 if node.pos.1 < nb.pos.1 {
                     above_count += 1;
                 } else if node.pos.1 > nb.pos.1 {
